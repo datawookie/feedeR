2df624d3a5da8c54320b1b2dd5d25554bea794c7
Andrew B. Collier <andrew@exegetic.biz>
2016-08-12 17:57:21 +0200

    Added test for new date/time format.

--- tests/testthat/test_util.R
+++ tests/testthat/test_util.R
@@ -17,3 +17,7 @@ test_that("handling of date/time like '2016-08-05T06:28:00-07:00'", {
 test_that("handling of date/time like '5 Aug 2016 13:28:00'", {
   expect_equal(parse.date("5 Aug 2016 13:28:00"), as.POSIXct(as.POSIXct("2016-08-05 13:28:00", tz = "UTC")))
 })
+
+test_that("handling of date/time like '5 Aug 2016 10:28:00 -0300'", {
+  expect_equal(parse.date("5 Aug 2016 10:28:00 -0300"), as.POSIXct(as.POSIXct("2016-08-05 13:28:00", tz = "UTC")))
+})
