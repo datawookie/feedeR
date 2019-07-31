3ffba155641e47c4be733c268f02b90c022da9a8
Andrew Collier <andrew@exegetic.biz>
2019-07-31 06:34:48 +0200

    Set User Agent String (some sites give 403 error with generic value).

--- R/read.R
+++ R/read.R
@@ -192,7 +192,13 @@ feed.read <- function(xml) {
 feed.extract <- function(url) {
   XMLFILE = tempfile(fileext = "-index.xml")
 
-  download.file(url = clean.url(url), XMLFILE, quiet = TRUE, method = "libcurl")
+  options(HTTPUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36")
+  #
+  download.file(
+    url = clean.url(url),
+    XMLFILE,
+    quiet = TRUE
+  )
 
   feed <- feed.read(XMLFILE)
 
